84c393cdeb41ae5dae02787e55770a887bcd7bf3
Nguyễn Thanh An <55699606+thanhanofhcmus@users.noreply.github.com>
2025-10-20 12:12:53 +0700

    [nvim] Change SnacksPickerDir to NvimLightGrey4

--- .config/nvim/lua/themes.lua
+++ .config/nvim/lua/themes.lua
@@ -6,3 +6,4 @@ vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'Keyword', { fg = 'NvimLightYellow', bold = false })
 
 vim.api.nvim_set_hl(0, 'SnacksIndentScope', { fg = 'white' })
+vim.api.nvim_set_hl(0, 'SnacksPickerDir', { fg = 'NvimLightGrey4' })
